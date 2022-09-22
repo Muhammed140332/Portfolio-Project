7282a6ef2e333074939197e227de7e1acc6038f6
Adekunle Muhammed <princeharmydex@gmail.com>
2022-09-22 12:25:44 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layouts/*.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        borderButton: 'rgba(225, 225, 225, 0.295743)',
+        butonTextColor: '#EF6D58',
+        hiColor: '#32334c',
+        firBackground:'#fcfcfc',
+        secBackground: '#fff7f3',
+        spanBackground: '#fdf0e9',
+        spanBorder: '#f3d1bf',
+        expColor: '#391400',
+        jeetarBackground: '#bce5ff',
+        probreak: 'rgba(57, 20, 0, 0.64)',
+        jeetar: '#f4f9ff',
+        woozeeeImg: '#ebf0f2',
+        prepBg: '#FFFCFB',
+        prepImg: 'rgba(229, 241, 255, 0.36)',
+        voixout: '#9B9B9B',
+        realpro: '#F7FBFF',
+        words: '#494A69',
+        last:'#E5E5E5'
+
+      }
+    },
+  },
+  plugins: [],
+}
